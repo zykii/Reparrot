991417f5d58907c8c9c7120bdcdb6662df901aab
zykii <109409131+zykii@users.noreply.github.com>
2023-02-13 19:55:23 -0800

    Add files via upload
    .NET FILES

---
+++ BlogAddRequest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.Blogs
+{
+    public class BlogAddRequest
+    {
+
+        [Required]
+        [Range(1, 3)]
+        public int BlogTypeId { get; set; }
+
+        
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Subject { get; set; }
+
+        [AllowNull]
+        public string Content { get; set; }
+
+        [AllowNull]
+        [Url]
+        [StringLength(255, MinimumLength = 2)]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public bool IsPublished { get; set; }
+
+        [AllowNull]
+        [DataType(DataType.DateTime)]
+        public DateTime DatePublish { get; set; }
+
+    }
+}
---
+++ BlogApiController.cs
@@ -0,0 +1,197 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sabio.Models;
+using Sabio.Models.Domain.Blogs;
+using Sabio.Models.Requests.Blogs;
+using Sabio.Services;
+using Sabio.Services.Interfaces;
+using Sabio.Web.Controllers;
+using Sabio.Web.Models.Responses;
+using System;
+using System.Collections.Generic;
+namespace Sabio.Web.Api.Controllers
+{
+    [Route("api/blogs")]
+    [ApiController]
+    public class BlogApiController : BaseApiController
+    {
+        private IBlogService _service = null;
+        private IAuthenticationService<int> _authService = null;
+        public BlogApiController(IBlogService service
+            , ILogger<BlogApiController> logger
+            , IAuthenticationService<int> authService) : base(logger)
+        {
+            _service = service;
+            _authService = authService;
+        }
+        [HttpGet("paginate")]
+        [AllowAnonymous]
+        public ActionResult<ItemResponse<Paged<Blog>>> GetAll(int pageIndex, int pageSize)
+        {
+            int code = 200;
+            BaseResponse response = null;
+            try
+            {
+                Paged<Blog> page = _service.GetAll(pageIndex, pageSize);
+                if (page == null)
+                {
+                    code = 404;
+                    response = new ErrorResponse("App Resource not found.");
+                }
+                else
+                {
+                    response = new ItemResponse<Paged<Blog>> { Item = page };
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+                base.Logger.LogError(ex.ToString());
+            }
+            return StatusCode(code, response);
+        }
+        [HttpGet("{id:int}")]
+        [AllowAnonymous]
+        public ActionResult<ItemResponse<Blog>> GetById(int id)
+        {
+            int iCode = 200;
+            BaseResponse response = null;
+            try
+            {
+                Blog blog = _service.GetById(id);
+                if (blog == null)
+                {
+                    iCode = 404;
+                    response = new ErrorResponse("Application Resource not found.");
+                }
+                else
+                {
+                    response = new ItemResponse<Blog> { Item = blog };
+                }
+            }
+            catch (Exception ex)
+            {
+                iCode = 500;
+                base.Logger.LogError(ex.ToString());
+                response = new ErrorResponse($"Generic Error: {ex.Message}");
+            }
+            return StatusCode(iCode, response);
+        }
+        [HttpPost]
+        public ActionResult<ItemResponse<int>> Create(BlogAddRequest model)
+        {
+            ObjectResult result = null;
+            try
+            {
+                int userId = _authService.GetCurrentUserId();
+                int id = _service.Add(model, userId);
+                ItemResponse<int> response = new ItemResponse<int>() { Item = id };
+                result = Created201(response);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.ToString());
+                ErrorResponse response = new ErrorResponse(ex.Message);
+                result = StatusCode(500, response);
+            }
+            return result;
+        }
+        [HttpPut("{id:int}")]
+        public ActionResult<SuccessResponse> Update(BlogUpdateRequest model)
+        {
+            int code = 200;
+            BaseResponse response = null;
+            try
+            {
+                int userId = _authService.GetCurrentUserId();
+                _service.Update(model, userId);
+                response = new SuccessResponse();
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+            }
+            return StatusCode(code, response);
+        }
+        [HttpDelete("{id:int}")]
+        public ActionResult<SuccessResponse> Delete(int id)
+        {
+            int code = 200;
+            BaseResponse response = null;
+            try
+            {
+                _service.Delete(id);
+                response = new SuccessResponse();
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+            }
+            return StatusCode(code, response);
+        }
+        [HttpGet("createdby")]
+        [AllowAnonymous]
+        public ActionResult<ItemResponse<Paged<Blog>>> GetCreatedBy(int pageIndex, int pageSize, int createdBy)
+        {
+            int code = 200;
+            BaseResponse response = null;
+            try
+            {
+                Paged<Blog> page = _service.GetByCreatedByPagination(pageIndex, pageSize, createdBy);
+                if (page == null)
+                {
+                    code = 404;
+                    response = new ErrorResponse("App Resource not found.");
+                }
+                else
+                {
+                    response = new ItemResponse<Paged<Blog>> { Item = page };
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+                base.Logger.LogError(ex.ToString());
+            }
+            return StatusCode(code, response);
+        }
+
+        [HttpGet("blogtype")]
+        [AllowAnonymous]
+        public ActionResult<ItemResponse<Paged<Blog>>> GetByBlogType(int pageIndex, int pageSize, int blogTypeId)
+        {
+            int code = 200;
+            BaseResponse response = null;
+            try
+            {
+                Paged<Blog> page = _service.GetByBlogType(pageIndex, pageSize, blogTypeId);
+
+                if (page == null)
+                {
+                    code = 404;
+                    response = new ErrorResponse("App Resource not found.");
+                }
+                else
+                {
+                    response = new ItemResponse<Paged<Blog>> { Item = page };
+                }
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+                base.Logger.LogError(ex.ToString());
+            }
+
+
+            return StatusCode(code, response);
+
+        }
+    }
+}
